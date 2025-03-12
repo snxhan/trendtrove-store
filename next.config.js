e87f80c1fbdbee1b90faefb37115d0ecd10dc7b7
Soh Xin Han <snxhan@gmail.com>
2025-03-12 19:03:24 +0800

    Update export

--- next.config.js
+++ next.config.js
@@ -31,6 +31,8 @@ const nextConfig = withStoreConfig({
       },
     ],
   },
+  // Enable static export
+  output: 'export',  // Add this line
 })
 
 console.log("next.config.js", JSON.stringify(module.exports, null, 2))
